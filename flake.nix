35b7c615df28efc9a98d237b42a46192a0bf923d
andrewkreuzer <me@andrewkreuzer.com>
2025-12-06 17:58:10 -0500

    grafana

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "age encryption";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+    treefmt-nix.url  = "github:numtide/treefmt-nix";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            age
+            sops
+          ];
+        };
+      }
+    );
+}
